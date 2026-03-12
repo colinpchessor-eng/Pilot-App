f1f9e4ddf6468b611cc3bd81223f461feea4d837
Colin Chessor <colinpchessor@gmail.com>
2026-03-12 15:54:06 +0000

    revert back one change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 import { FirebaseClientProvider } from '@/firebase/client-provider';
+import { Inter, Orbitron } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'], variable: '--font-body' });
+const orbitron = Orbitron({ subsets: ['latin'], variable: '--font-headline' });
 
 export const metadata: Metadata = {
   title: 'FedexFlow',
@@ -14,15 +18,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
-      </head>
+    <html lang="en" className={`dark ${inter.variable} ${orbitron.variable}`}>
+      <head />
       <body className="font-body antialiased">
         <FirebaseClientProvider>{children}</FirebaseClientProvider>
         <Toaster />
