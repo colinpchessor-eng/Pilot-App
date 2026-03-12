12554702ebc13ddb71f554d784660c1be1347ee3
Colin Chessor <colinpchessor@gmail.com>
2026-03-12 15:49:18 +0000

    change  to this font-family: 'Montserrat', 'Futura', sans-serif;

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['Inter', 'sans-serif'],
-        headline: ['Inter', 'sans-serif'],
+        body: ['Montserrat', 'Futura', 'sans-serif'],
+        headline: ['Montserrat', 'Futura', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
